9bdbcf44c77964faf18493395fba76234e487728
xvmvx <107462385+xvmvx@users.noreply.github.com>
2022-11-25 21:13:16 +0800

    Create bt.sh

---
+++ bt.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget -O install.sh http://f.cccyun.cc/bt/install_6.0.sh && bash install.sh
+sed -i "s|bind_user == 'True'|bind_user == 'XXXX'|" /www/server/panel/BTPanel/static/js/index.js
+rm -f /www/server/panel/data/bind.pl && curl -sSO https://raw.githubusercontent.com/ztkink/bthappy/main/one_key_happy.sh && bash one_key_happy.sh
+wget -O optimize.sh http://f.cccyun.cc/bt/optimize.sh && bash optimize.sh && curl https://io.bt.sy/install/update_panel.sh|bash
