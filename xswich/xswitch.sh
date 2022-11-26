c720574a3c0e69adc678716c24c8678972836e67
xvmvx <107462385+xvmvx@users.noreply.github.com>
2022-11-27 00:08:40 +0800

    Create xswitch.sh

---
+++ xswich/xswitch.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+sudo apt-get update && sudo apt-get install -y make zip wget
+wget https://xswitch.cn/download/xswitch-install.tar.gz --user xswitch --password password
+tar zxvf xswitch-install.tar.gz 
+cp xswitch-install && cd xswitch-install
+
+
