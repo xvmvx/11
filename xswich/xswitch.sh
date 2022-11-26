c8b75432255b6149a534d8eb5879c650736d5899
xvmvx <107462385+xvmvx@users.noreply.github.com>
2022-11-27 01:58:32 +0800

    Update xswitch.sh

--- xswich/xswitch.sh
+++ xswich/xswitch.sh
@@ -1,7 +1,8 @@
-#!bin/bash
+#!/bin/bash
 sudo apt-get update && sudo apt-get install -y make zip wget
 wget https://xswitch.cn/download/xswitch-install.tar.gz --user xswitch --password password
-tar zxvf xswitch-install.tar.gz 
-cp xswitch-install && cd xswitch-install
+tar zxvf xswitch-install.tar.gz && cd xswitch-install
+make setup
+vim .env
 
 
