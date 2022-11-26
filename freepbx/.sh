d274677f9c820891e09487e55017d3eb12447163
xvmvx <107462385+xvmvx@users.noreply.github.com>
2022-11-27 04:18:53 +0800

    Update .sh

--- freepbx/.sh
+++ freepbx/.sh
@@ -2,8 +2,9 @@
 export PATH=$PATH:~/bin
 source ~/.bashrc
 set -Eeuxo pipefail
-git clone https://github.com/ugoviti/izpbx.git /izpbx && cd /izpbx
-echo "进程:" pstree -aup
-echo "内网:" ip addr | grep inet
-echo "外网:" curl ip.sb
+git clone https://github.com/ugoviti/izpbx.git && cd /izpbx
+ring = pstree -aup
+echo -n "进程:"; echo pstree -aup
+echo -n "内网:"; echo ip addr | grep inet
+echo -n "外网:"; echo  curl ip.sb
 cp default.env .env && vim .env 
