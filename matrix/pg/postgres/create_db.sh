c0c8f9a1c1fad72f0ae46678bcf1d4a7fee58436
xvmvx <107462385+xvmvx@users.noreply.github.com>
2022-11-28 16:35:33 +0800

    Create create_db.sh

---
+++ matrix/pg/postgres/create_db.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+createdb -U synapse_user -O synapse_user --encoding=UTF8 --locale=C --template=template0 synapse
+createdb -U synapse_user -O synapse_user matrix_qq
+createdb -U synapse_user -O synapse_user matrix_wechat
