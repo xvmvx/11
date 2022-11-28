fcc6e3208551cf12dab4fa6f106a9e5ad2a5b59b
xvmvx <107462385+xvmvx@users.noreply.github.com>
2022-11-28 16:40:14 +0800

    Update synapse.sh

--- matrix/synapse/synapse.sh
+++ matrix/synapse/synapse.sh
@@ -2,3 +2,9 @@
 mkdir -p synapse/logs
 mkdir synapse/media_store
 mkdir synapse/uploads
+each "docker run --rm \
+	-v `pwd`/synapse:/data \
+	-e SYNAPSE_SERVER_NAME=修改为你的网址 \
+	-e SYNAPSE_REPORT_STATS=no \
+	matrixdotorg/synapse:latest generate
+"
