d6622fdaa2f67fabf09b1b340b3fb414968098d0
xvmvx <107462385+xvmvx@users.noreply.github.com>
2022-12-11 22:06:33 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,30 +1,97 @@
 #!/bin/bash
-#彩色
-red(){echo -e "\033[31m\033[01m$1\033[0m"}
-green(){echo -e "\033[32m\033[01m$1\033[0m"}
-yellow(){echo -e "\033[33m\033[01m$1\033[0m"}
-blue(){echo -e "\033[34m\033[01m$1\033[0m"}
+echo "#######################################################################"
+xitong=$(cat /etc/issue)
+CentOS="CentOS"
+echo -n "本机系统是：" ; echo ${xitong%(Final)*}
+result=$(echo $xitong | grep "${CentOS}")
+if [[ "$result" != "" ]]
+then
+    echo "本机IP请使用ifconfig -a 检测"
+else
+    echo -n "本机IP是："; curl ip.sb
+fi
+alias find="find . -name"
 
-#主菜单
-function start_menu(){
-    clear
-    red "#######################################################################
-    red "#                                                                     #
-    red "#                          My 工具箱                                   #
-    red "#                                                                     #
-    red "####################################################################### 
-  green "======================================================================="
-  green "=========           X86系统 请按y;RAM系统 请按n;其他系统暂不支持   ========="
-  green "======================================================================="    
-  echo
-  read -p "请输入选择:" menuNumberInput
+echo "本机已启用的命令别名"
+alias
+echo "alias显示全部别名，unalias lt删除别名"
+echo "======================================================================"
+echo "本机服务商信息请vim修改/etc/profile文件的末尾添加"
+echo "本机已启用：VOS3000"
+echo "#######################################################################"
+echo "1.   运行   综合工具箱"
+echo "2.   运行   杜甫测试"
+echo "3.   DD 推荐     MoeClub脚本"
+echo "31.  甲骨文   DD成Debian 10"
+echo "32.  甲骨文   DD成Ubuntu 20.04"
+echo "33.  DD脚本"
+echo "       测试      "
+echo "41.   单线程测试"
+echo "42.   最全测速脚本"
+echo "43.   superbench"
+echo "44.   Bench.sh"
+echo "       速度测试      "
+echo "45.   显示延迟、抖动"
+echo "46.   直接显示回程线路"
+echo "47.   直接显示回程线路2"
+echo "48.   四网测速"
+echo "49.   三网测速"
+echo "42.   "
+echo "42.   "
+echo "42.   
+read -p "退出请回车" menuNumberInput
   case "$menuNumberInput" in
-    y )
-      x86
+    1 )
+      	wget -O box.sh https://raw.githubusercontent.com/BlueSkyXN/SKY-BOX/main/box.sh && chmod +x box.sh && clear && ./box.sh
     ;;
-    n )
-      ram
+    2 )
+      	wget -q https://github.com/Aniverse/A/raw/i/a && bash a
     ;;
+    3 )
+    	bash <(wget --no-check-certificate -qO- 'https://raw.githubusercontent.com/MoeClub/Note/master/InstallNET.sh') -d 10 -v 64 -p "自定义root密码" -port "自定义ssh端口"
+    ;;
+    31 )
+	bash <(wget --no-check-certificate -qO- 'https://raw.githubusercontent.com/MoeClub/Note/master/InstallNET.sh') -d 10 -v 64 -p "自定义root密码" -port "自定义ssh端口"
+    ;;
+    32 )
+    	bash <(wget --no-check-certificate -qO- 'https://raw.githubusercontent.com/MoeClub/Note/master/InstallNET.sh') -u 20.04 -v 64 -p "自定义root密码" -port "自定义ssh端口"
+    ;;
+    33 )
+    	wget --no-check-certificate -O AutoReinstall.sh https://git.io/AutoReinstall.sh && bash AutoReinstall.sh
+    ;;
+    41 )
+	bash <(curl -Lso- https://bench.im/hyperspeed)
+    ;;
+    42 )
+    	curl -fsL https://ilemonra.in/LemonBenchIntl | bash -s fast
+    ;;
+    43 )
+        wget -qO- git.io/superbench.sh | bash
+    ;;
+    44 )
+    	wget -qO- bench.sh | bash
+    ;;
+    45 )
+ 	bash <(wget -qO- https://bench.im/hyperspeed)
+    ;;
+    46 )
+    	curl https://raw.githubusercontent.com/zhucaidan/mtr_trace/main/mtr_trace.sh|bash || wget https://raw.githubusercontent.com/nanqinlang-script/testrace/master/testrace.sh
+bash testrace.sh
+    ;;
+    47 )
+    wget -q route.f2k.pub -O route && bash route || wget -qO- git.io/besttrace | bash
+    ;;
+    48 )
+    	wget -O jcnf.sh https://raw.githubusercontent.com/Netflixxp/jcnfbesttrace/main/jcnf.sh && bash jcnf.sh
+    ;;
+    49 ）
+    	bash <(curl -Lso- https://git.io/superspeed_uxh)
+
+
+        ;;
+    2 )
+
+
     * )
       clear
       red "请输入正确数字 !"
