69eb1aa171da83c4dcbb760c09930a2ce26b79b3
xvmvx <107462385+xvmvx@users.noreply.github.com>
2022-12-12 00:21:04 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -10,29 +10,22 @@ then
 else
     echo -n "本机IP是："; curl ip.sb
 fi
+echo "本机服务商信息:请vim修改/etc/profile文件的末尾添加"
+echo "本机运行服务信息：已启用   VOS3000"
+echo "======================================================================"
 alias find="find . -name"
-
 echo "本机已启用的命令别名"
 alias
 echo "alias显示全部别名，unalias lt删除别名"
 echo "======================================================================"
-echo "本机服务商信息:请vim修改/etc/profile文件的末尾添加"
-echo "本机运行服务信息：已启用   VOS3000"
+echo "               My goodway "
 echo "#######################################################################"
 echo " "
 echo " "
 echo " "
-echo "======================================================================"
-echo "               My goodway "
-echo "1.修改本机服务商信息"
-echo "2.修改本机运行服务信息"
-echo "3.运行综合工具箱"
-echo "4.运行超级工具箱"
-echo "5. 性能测试 综合测试 视频测试"
-echo "6. 网络测试 速度测试"
-echo "7. DD WARP BBR"
-echo "8. Docker"
-echo "9. 其他"
+echo "1.添加显示本窗口  2.修改本机服务商信息和运行服务信息"
+echo "3.运行综合工具箱      4.运行超级工具箱    5. 性能测试 综合测试 视频测试"
+echo "6. 网络测试 速度测试      7. DD WARP BBR      8. Docker和其他"
 read -p "退出请回车" menuNumberInput
   case "$menuNumberInput" in
     1 )
@@ -48,7 +41,7 @@ read -p "退出请回车" menuNumberInput
     	
     ;;
     5 )
-	      source 15.sh
+	source 15.sh
     ;;
     6 )
        source 16.sh
@@ -67,4 +60,3 @@ read -p "退出请回车" menuNumberInput
       esac
     ;;
     esac
-}
