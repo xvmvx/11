ab64970fca3a46001f1a0a190f43a8519955a1ab
xvmvx <107462385+xvmvx@users.noreply.github.com>
2022-12-16 01:55:31 +0800

    Update pro.sh

--- pro.sh
+++ pro.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-if [ ! -d "~/1" ]; then
-  sudo git clone https://github.com/xvmvx/1.git /root/1
-else
-  sudo rm -rf ~/1
-  sudo git clone https://github.com/xvmvx/1.git /root/1
-fi
+sudo rm -rf /root/1 && sudo git clone https://github.com/xvmvx/1.git /root/1
