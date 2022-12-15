b9d889606e7061989c1912f6814451857664a7ab
xvmvx <107462385+xvmvx@users.noreply.github.com>
2022-12-16 02:44:51 +0800

    Update xswitch.sh

--- xswich/xswitch.sh
+++ xswich/xswitch.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install -y make zip wget
-wget https://xswitch.cn/download/xswitch-install.tar.gz --user xswitch --password password
+sudo apt-get update
+sudo apt-get install -y make zip wget || sudo yum install -y make zip wget
+sudo wget https://xswitch.cn/download/xswitch-install.tar.gz --user xswitch --password password
 tar zxvf xswitch-install.tar.gz && cd xswitch-install
-make setup 
-vim .env
+make setup && mv .env .env.beifen
+cp ../.env .env
+make up
 
 
