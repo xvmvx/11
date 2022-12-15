162d039b4a9db022f0925aa90ee0e91c792421d4
xvmvx <107462385+xvmvx@users.noreply.github.com>
2022-12-16 02:33:39 +0800

    Update pro.sh

--- pro.sh
+++ pro.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo rm -rf /root/1 
-sudo git clone https://github.com/xvmvx/1.git /root/1
+rm -rf /root/1 
+git clone https://github.com/xvmvx/1.git /root/1
 chmod +x /root/1/*
 source /root/1/1.sh
